8c20d272e077b15fc8af77cff366525de92d748f
Raissa K <raissakuzerdev@gmail.com>
2022-09-18 10:11:40 -0300

    Substituted node-cache for apicache. (#30)
    Also updated branch from issue name template.

--- controllers/home.js
+++ controllers/home.js
@@ -1,21 +1,10 @@
 const fetch = require('cross-fetch')
-const NodeCache = require('node-cache')
-const cache = new NodeCache({ stdTTL: 3600 });
 
 module.exports = {
     getIndex: async (req,res)=>{
-        if (cache.has('indexPicture')){
-            res.render('index.ejs', {pictureUrl: `${cache.get('indexPicture')+'&crop=edges&w=700&h=500&fit=crop'}`})
-        } else {
-            try {
-                let response = await fetch(`https://api.unsplash.com/photos/random/?query=pet&client_id=${process.env.UNSPLASH_CLIENT_ID}`)
-                const data = await response.json()
-                let picture = data.urls.raw
-                cache.set('indexPicture', picture)
-                res.render('index.ejs', {pictureUrl: `${cache.get('indexPicture')+'&crop=edges&w=700&h=500&fit=crop'}`})                
-            } catch (error) {
-                console.error(error)
-            }
+        const response = await fetch(`https://api.unsplash.com/photos/random/?query=pet&client_id=${process.env.UNSPLASH_CLIENT_ID}`)
+        const data = await response.json()
+        let picture = data.urls.raw
+        res.render('index.ejs', {pictureUrl: `${picture+'&crop=edges&w=700&h=500&fit=crop'}`})
         }
     }
-}
