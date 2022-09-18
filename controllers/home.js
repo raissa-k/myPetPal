e9f67902bd24ec3f6cf0dea28b6be36ed1c55a9f
Raissa K <raissakuzerdev@gmail.com>
2022-09-18 18:28:40 -0300

    Added unsplash.js
    Now using unsplash.js to handle Unsplash's API requests and JSON.
    - Uses node-fetch
    
    Adjusted index.ejs to properly credit the author's photo according to
    Unsplash's API guidelines.

--- controllers/home.js
+++ controllers/home.js
@@ -1,12 +1,20 @@
 const fetch = require('node-fetch')
+global.fetch = fetch
+const { createApi } = require('unsplash-js')
+require('dotenv').config({ path: './config/.env' })
+
+const unsplash = createApi({
+    accessKey: process.env.UNSPLASH_CLIENT_ID
+})
 
 module.exports = {
     getIndex: async (req,res)=>{
-        let picture
-        await fetch(`https://api.unsplash.com/photos/random/?query=pet&client_id=${process.env.UNSPLASH_CLIENT_ID}`)
-        .then(response => response.json())
-        .then(data => picture = data.urls.raw)
-        .catch(err => console.error(err))
-        res.render('index.ejs', {pictureUrl: picture+'&crop=edges&w=700&h=500&fit=crop'})            
+        let data
+        await unsplash.photos.getRandom({ query: 'pet', orientation: 'landscape', count: 1})
+        .then(result => {
+            data = result.response[0]
+        })
+        .catch(error => console.error(error))
+        res.render('index.ejs', {datapicture: `${data.urls.raw}&fm=jpg&crop=edges&fit=crop&w=700&h=500&q=80&fit=max`, data: data})            
     }
 }
