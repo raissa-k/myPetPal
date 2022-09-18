5b20455b4b17c837b565271faf4f62c58dea08dc
Raissa K <raissakuzerdev@gmail.com>
2022-09-18 03:12:47 -0300

    Added trycatch block to home.js for Render

--- controllers/home.js
+++ controllers/home.js
@@ -7,11 +7,15 @@ module.exports = {
         if (cache.has('indexPicture')){
             res.render('index.ejs', {pictureUrl: `${cache.get('indexPicture')+'&crop=edges&w=700&h=500&fit=crop'}`})
         } else {
-            let response = await fetch(`https://api.unsplash.com/photos/random/?query=pet&client_id=${process.env.UNSPLASH_CLIENT_ID}`)
-            const data = await response.json()
-            let picture = data.urls.raw
-            cache.set('indexPicture', picture)
-            res.render('index.ejs', {pictureUrl: `${cache.get('indexPicture')+'&crop=edges&w=700&h=500&fit=crop'}`})
+            try {
+                let response = await fetch(`https://api.unsplash.com/photos/random/?query=pet&client_id=${process.env.UNSPLASH_CLIENT_ID}`)
+                const data = await response.json()
+                let picture = data.urls.raw
+                cache.set('indexPicture', picture)
+                res.render('index.ejs', {pictureUrl: `${cache.get('indexPicture')+'&crop=edges&w=700&h=500&fit=crop'}`})                
+            } catch (error) {
+                console.error(error)
+            }
         }
     }
 }
